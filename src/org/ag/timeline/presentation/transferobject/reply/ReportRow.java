e2e3f48e91bbb64b90a26918a7baea565a8686e2
abhishek.gaurav <ag.java@gmail.com>
2013-11-30 17:08:19 +0530

    Fixed javadoc.

--- src/org/ag/timeline/presentation/transferobject/reply/ReportRow.java
+++ src/org/ag/timeline/presentation/transferobject/reply/ReportRow.java
@@ -6,11 +6,6 @@ package org.ag.timeline.presentation.transferobject.reply;
  * 
  * @author Abhishek Gaurav
  */
-/**
- * 
- * 
- * @author Abhishek Gaurav
- */
 public class ReportRow {
 
 	private long projectId = 0;
