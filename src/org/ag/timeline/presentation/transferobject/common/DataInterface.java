d97add0ac08a9a69f3df70a9bcdafe594b365a95
abhishek.gaurav <ag.java@gmail.com>
2014-01-06 06:52:23 +0530

    Added Marker interface for handling serializable transfer objects.

---
+++ src/org/ag/timeline/presentation/transferobject/common/DataInterface.java
@@ -0,0 +1,12 @@
+package org.ag.timeline.presentation.transferobject.common;
+
+import java.io.Serializable;
+
+/**
+ * Interface for data objects.
+ * 
+ * @author Abhishek Gaurav
+ */
+public interface DataInterface extends Serializable {
+
+}
