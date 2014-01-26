4ef8e3d6b24912fd7f277fd76a0efbcc6fe230e1
abhishek.gaurav <ag.java@gmail.com>
2014-01-26 19:49:54 +0530

    Added new feature support.

--- src/org/ag/timeline/business/service/iface/TimelineService.java
+++ src/org/ag/timeline/business/service/iface/TimelineService.java
@@ -9,22 +9,15 @@ import org.ag.timeline.presentation.transferobject.input.CodeValueInput;
 import org.ag.timeline.presentation.transferobject.input.ProjectEstimatesInput;
 import org.ag.timeline.presentation.transferobject.input.ProjectInput;
 import org.ag.timeline.presentation.transferobject.input.ProjectMetricsInput;
-import org.ag.timeline.presentation.transferobject.input.ProjectStageInput;
-import org.ag.timeline.presentation.transferobject.input.ProjectStageTaskInput;
 import org.ag.timeline.presentation.transferobject.input.StatusInput;
+import org.ag.timeline.presentation.transferobject.input.TaskInput;
 import org.ag.timeline.presentation.transferobject.input.TimeDataInput;
 import org.ag.timeline.presentation.transferobject.input.UserInput;
 import org.ag.timeline.presentation.transferobject.input.UserPreferencesInput;
 import org.ag.timeline.presentation.transferobject.reply.ActivityReply;
-import org.ag.timeline.presentation.transferobject.reply.AuditDataReply;
-import org.ag.timeline.presentation.transferobject.reply.CodeValueListReply;
 import org.ag.timeline.presentation.transferobject.reply.CodeValueReply;
-import org.ag.timeline.presentation.transferobject.reply.DetailedReportReply;
-import org.ag.timeline.presentation.transferobject.reply.ProjectDetailMetricsReply;
-import org.ag.timeline.presentation.transferobject.reply.ProjectEstimatesReply;
-import org.ag.timeline.presentation.transferobject.reply.ProjectLevelMetricsReply;
 import org.ag.timeline.presentation.transferobject.reply.ProjectReply;
-import org.ag.timeline.presentation.transferobject.reply.SummaryReportReply;
+import org.ag.timeline.presentation.transferobject.reply.TaskReply;
 import org.ag.timeline.presentation.transferobject.reply.TimeDataReply;
 import org.ag.timeline.presentation.transferobject.reply.UserPreferenceReply;
 import org.ag.timeline.presentation.transferobject.reply.UserPreferenceSearchReply;
@@ -32,14 +25,20 @@ import org.ag.timeline.presentation.transferobject.reply.UserReply;
 import org.ag.timeline.presentation.transferobject.reply.UserSearchReply;
 import org.ag.timeline.presentation.transferobject.reply.WeekReply;
 import org.ag.timeline.presentation.transferobject.reply.WeeklyUserReply;
+import org.ag.timeline.presentation.transferobject.reply.agile.RapidBoardReply;
+import org.ag.timeline.presentation.transferobject.reply.audit.AuditDataReply;
+import org.ag.timeline.presentation.transferobject.reply.metrics.ProjectDetailMetricsReply;
+import org.ag.timeline.presentation.transferobject.reply.metrics.ProjectEstimatesReply;
+import org.ag.timeline.presentation.transferobject.reply.metrics.ProjectLevelMetricsReply;
+import org.ag.timeline.presentation.transferobject.reply.report.DetailedReportReply;
+import org.ag.timeline.presentation.transferobject.reply.report.SummaryReportReply;
 import org.ag.timeline.presentation.transferobject.search.ActivitySearchParameter;
 import org.ag.timeline.presentation.transferobject.search.AuditDataSearchParameters;
 import org.ag.timeline.presentation.transferobject.search.ProjectDetailMetricsSearchParameters;
 import org.ag.timeline.presentation.transferobject.search.ProjectMetricsSearchParameters;
 import org.ag.timeline.presentation.transferobject.search.ProjectSearchParameter;
-import org.ag.timeline.presentation.transferobject.search.ProjectStageSearchParameters;
 import org.ag.timeline.presentation.transferobject.search.ReportSearchParameters;
-import org.ag.timeline.presentation.transferobject.search.StageSearchParameter;
+import org.ag.timeline.presentation.transferobject.search.TaskSearchParameter;
 import org.ag.timeline.presentation.transferobject.search.TimeDataSearchParameters;
 import org.ag.timeline.presentation.transferobject.search.UserPreferenceSearchParameter;
 import org.ag.timeline.presentation.transferobject.search.UserSearchParameter;
@@ -60,9 +59,7 @@ public interface TimelineService {
 	public UserReply createUser(UserInput input) throws TimelineException;
 	public CodeValueReply createTimeData(final TimeDataInput myTimeData) throws TimelineException;
 	public CodeValueReply createProjectDetailMetrics(final ProjectMetricsInput input) throws TimelineException;
-	public CodeValueReply createStage(final CodeValueInput input) throws TimelineException;
-	public CodeValueReply createProjectStage(final ProjectStageInput input) throws TimelineException;
-	public CodeValueReply createProjectStageTask(final ProjectStageTaskInput input) throws TimelineException;
+	public CodeValueReply createTask(final TaskInput input) throws TimelineException;
 	
 	public CodeValueReply deleteProject(CodeValueInput input) throws TimelineException;
 	public CodeValueReply deleteActivity(CodeValueInput input) throws TimelineException;
@@ -71,9 +68,7 @@ public interface TimelineService {
 	public CodeValueReply deleteUserPreferences(CodeValueInput input) throws TimelineException;
 	public CodeValueReply deleteProjectMetrics(final CodeValueInput input) throws TimelineException;
 	public CodeValueReply deleteProjectDetailMetrics(final CodeValueInput input) throws TimelineException;
-	public CodeValueReply deleteStage(final CodeValueInput input) throws TimelineException;
-	public CodeValueReply deleteProjectStage(final CodeValueInput input) throws TimelineException;
-	public CodeValueReply deleteProjectStageTask(final CodeValueInput input) throws TimelineException;
+	public CodeValueReply deleteTask(final CodeValueInput input) throws TimelineException;
 		
 	public CodeValueReply modifyActivity(CodeValueInput input) throws TimelineException;
 	public CodeValueReply modifyProject(ProjectInput input) throws TimelineException;
@@ -81,7 +76,7 @@ public interface TimelineService {
 	public CodeValueReply modifyTimeData(final TimeDataInput myTimeData) throws TimelineException;
 	public CodeValueReply modifyStatus(final StatusInput input) throws TimelineException;
 	public CodeValueReply modifyProjectDetailMetrics(final ProjectMetricsInput input) throws TimelineException;
-	public CodeValueReply modifyProjectStageTask(final ProjectStageTaskInput input) throws TimelineException;
+	public CodeValueReply modifyTask(final TaskInput input) throws TimelineException;
 	
 	public UserPreferenceReply saveUserPreferences(final UserPreferencesInput input) throws TimelineException;
 	public CodeValueReply saveProjectEstimates(final ProjectEstimatesInput input) throws TimelineException;
@@ -95,14 +90,14 @@ public interface TimelineService {
 	public AuditDataReply searchAuditData(final AuditDataSearchParameters searchParameters) throws TimelineException;
 	public WeeklyUserReply searchUsersWithoutEntries(final WeekSearchParameter searchParameters) throws TimelineException;
 	public ProjectEstimatesReply searchProjectEstimates(final ProjectSearchParameter searchParameters) throws TimelineException;
-	public CodeValueListReply searchTasks(final ProjectSearchParameter searchParameters) throws TimelineException;
-	public CodeValueListReply searchStages(final StageSearchParameter searchParameters) throws TimelineException;
-	public CodeValueListReply searchProjectStage(final ProjectStageSearchParameters searchParameters) throws TimelineException;
+	public TaskReply searchTasks(final TaskSearchParameter searchParameters) throws TimelineException;
 	
 	public SummaryReportReply getSummaryReport(final ReportSearchParameters searchParameters) throws TimelineException;
 	public DetailedReportReply getDetailedReport(final ReportSearchParameters searchParameters) throws TimelineException;
 	public ProjectLevelMetricsReply getProjectLevelMetricsReport(final ProjectMetricsSearchParameters searchParameters) throws TimelineException;
 	public ProjectDetailMetricsReply getProjectDetailMetricsReport(final ProjectDetailMetricsSearchParameters searchParameters) throws TimelineException;
+	
+	public RapidBoardReply getRapidBoardData(final ProjectSearchParameter searchParameters) throws TimelineException;
 
 	public void systemManagement() throws TimelineException;
 
