c0a06d19e48ea0ea5b33fa3b1df63d2ef1ef50a9
abhishek.gaurav <ag.java@gmail.com>
2014-01-26 19:58:14 +0530

    Added new feature support.

---
+++ src/org/ag/timeline/presentation/transferobject/reply/agile/RapidBoardReply.java
@@ -0,0 +1,145 @@
+/**
+ * 
+ */
+package org.ag.timeline.presentation.transferobject.reply.agile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.ag.timeline.business.model.Activity;
+import org.ag.timeline.business.model.Project;
+import org.ag.timeline.business.model.Task;
+import org.ag.timeline.presentation.transferobject.common.CodeValue;
+import org.ag.timeline.presentation.transferobject.reply.BusinessReply;
+import org.ag.timeline.presentation.transferobject.reply.ProjectData;
+
+/**
+ * Contains Rapid Board related data.
+ * 
+ * @author Abhishek Gaurav
+ */
+public class RapidBoardReply extends BusinessReply {
+
+	private ProjectData projectData = null;
+
+	private Map<Long, String> activityMap = null;
+
+	private Map<Long, List<CodeValue>> activityTaskMap = null;
+
+	/**
+	 * Default Constructor
+	 */
+	public RapidBoardReply() {
+
+		this.projectData = new ProjectData();
+		this.activityMap = new HashMap<Long, String>();
+		this.activityTaskMap = new HashMap<Long, List<CodeValue>>();
+	}
+
+	/**
+	 * Getter for projectData.
+	 * 
+	 * @return the projectData.
+	 */
+	public ProjectData getProjectData() {
+		return this.projectData;
+	}
+
+	/**
+	 * Returns a list of {@link Activity} ids.
+	 * 
+	 * @return List<Long>.
+	 */
+	public List<Long> getActivityIdList() {
+		List<Long> list = null;
+
+		if ((this.activityMap != null) && (this.activityMap.keySet() != null)) {
+			list = new ArrayList<Long>();
+			list.addAll(this.activityMap.keySet());
+		}
+
+		return list;
+	}
+
+	public List<CodeValue> getActivityTasks(long activityId) {
+		List<CodeValue> list = null;
+
+		if ((this.activityTaskMap != null) && (this.activityTaskMap.keySet() != null)) {
+			list = this.activityTaskMap.get(activityId);
+		}
+
+		return list;
+	}
+
+	public String getActivityName(long activityId) {
+		String name = null;
+
+		if (this.activityMap != null) {
+			name = this.activityMap.get(activityId);
+		}
+
+		return name;
+	}
+
+	/**
+	 * Adds a {@link Project} to this reply.
+	 * 
+	 * @param project {@link Project} owing the current
+	 *            {@link ProjectStageTaskReply} object.
+	 */
+	public void addProject(Project project) {
+		this.projectData.setCode(project.getId());
+		this.projectData.setValue(project.getName());
+	}
+
+	public void addActivity(Activity activity) {
+		this.activityMap.put(activity.getId(), activity.getName());
+	}
+
+	public boolean hasTasks() {
+
+		boolean retVal = false;
+
+		if ((this.activityTaskMap != null) && (this.activityTaskMap.values() != null)
+				&& (this.activityTaskMap.values().size() > 0)) {
+			retVal = true;
+		}
+
+		return retVal;
+	}
+
+	public boolean hasProjectStages() {
+
+		boolean retVal = false;
+
+		if ((this.activityMap != null) && (this.activityMap.values() != null) && (this.activityMap.values().size() > 0)) {
+			retVal = true;
+		}
+
+		return retVal;
+	}
+
+	public void addActivityTask(Task task) {
+
+		if (task != null) {
+
+			Activity activity = task.getActivity();
+
+			if (activity != null) {
+
+				final long activityId = activity.getId();
+				List<CodeValue> taskList = this.activityTaskMap.get(activityId);
+
+				if (taskList == null) {
+					taskList = new ArrayList<CodeValue>();
+				}
+
+				taskList.add(new CodeValue(task.getId(), task.getText()));
+				this.activityTaskMap.put(activityId, taskList);
+				this.activityMap.put(task.getActivity().getId(), task.getActivity().getName());
+			}
+		}
+	}
+}
