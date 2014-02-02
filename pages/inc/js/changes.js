f211b59a13960b3b30149db73d07a55b073fe90c
abhishek.gaurav <ag.java@gmail.com>
2014-02-02 19:31:25 +0530

    Fixed Change search bug.

--- pages/inc/js/changes.js
+++ pages/inc/js/changes.js
@@ -7,4 +7,46 @@ $(function() {
 	    active : 0
 	});
 	
+	$('#fromDateImg').click(function() {
+		$('#fromDate').datepicker('show');
+	});
+
+	$('#toDateImg').click(function() {
+		$('#toDate').datepicker('show');
+	});
+	
+	
+	//set the initial dates
+	{
+		var currentDate = new Date();
+		var from = $('#fromDate').val();
+		var to = $('#toDate').val();
+
+		if ((from == null) || (from == "")) {
+			$('#fromDate').val($.datepicker.formatDate('dd M yy', currentDate));
+		}
+
+		if ((to == null) || (to == "")) {
+			$('#toDate').val($.datepicker.formatDate('dd M yy', currentDate));
+		}
+	}
+	
+	$('#fromDate').datepicker({
+	    showOtherMonths : true,
+	    selectOtherMonths : true,
+	    changeMonth : true,
+	    changeYear : true,
+	    firstDay : 1,
+	    dateFormat : "dd M yy"
+	});
+	
+	$('#toDate').datepicker({
+	    showOtherMonths : true,
+	    selectOtherMonths : true,
+	    changeMonth : true,
+	    changeYear : true,
+	    firstDay : 1,
+	    dateFormat : "dd M yy"
+	});
+	
 });
